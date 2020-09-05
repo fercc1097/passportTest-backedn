9c9345a0dd882a00dd055808c3d6f328a312e747
fercc1097 <fercc1097@gmail.com>
2020-09-04 23:24:21 -0500

    mongo connected succesfully

--- server.js
+++ server.js
@@ -8,8 +8,19 @@ const bcrypt = require('bcryptjs');
 const expressSessions = require('express-session');
 const bodyParser = require('body-parser');
 const sessions = require('express-session');
+const User = require('./user');
+const mongoPass = require('./foo');
 
 const app = express();
+
+mongoose.connect(
+    `mongodb+srv://passport:${mongoPass}@cluster0.hhwfo.mongodb.net/<dbname>?retryWrites=true&w=majority`,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    },
+    () => console.log('Mongoose is connected succesfully')
+)
 
 //Middleware 
 
@@ -28,9 +39,23 @@ app.post("/login", (req,res) => {
     console.log(req.body);
 })
 app.post("/register", (req,res) => {
-    console.log(req.body);
+    User.findOne({username: req.body.username}, async (err,doc) => {
+        if(err) throw err;
+        if(doc) res.send('User already exists');
+        if(!doc) {
+            console.log(req.body)
+            const newUser = new User({
+                username: req.body.username,
+                password: req.body.password,
+            });
+            await newUser.save();
+            res.send("User created succesfully");
+        }
+    })
 })
-app.get("/user", (req,res) => {})
+app.get("/user", (req,res) => {
+
+})
 
 app.use(cookieParser("secretcode"));
 
